9a0934e1a3698ef1f490f62d26e49607d04d8cb6
mperezi <mperezibars@gmail.com>
2021-04-26 07:19:48 +0200

    Extract repo module

--- src/api.py
+++ src/api.py
@@ -1,45 +1,21 @@
-import boto3
 import json
 import logging
 import string
-from src.validators import url as validate_url
+import src.repo as repo
+
 from collections import defaultdict
 from random import choice
 from itertools import chain
 from botocore.exceptions import ClientError
-from os import environ as env
+from src.validators import url as validate_url
 
-s3_client = boto3.client('s3')
+
 logger = logging.getLogger()
-bucket_name = env['BUCKET_NAME']
-
-
-def create_redirect_object(path, origin_url):
-    return {
-        'Bucket': bucket_name,
-        'Key': path,
-        'WebsiteRedirectLocation': origin_url
-    }
-
-
-def upload_redirect_object(redirect):
-    s3_client.put_object(**redirect)
-
-
-def is_path_free(path):
-    try:
-        s3_client.head_object(Bucket=bucket_name, Key=path)
-        return False
-    except ClientError as ex:
-        error_code = ex.response['Error']['Code']
-        if error_code == '404':
-            return True
-        raise ex
 
 
 def generate_path(len):
     new_path = ''.join(choice(list(chain(string.ascii_letters, string.digits))) for c in range(len))
-    if is_path_free(new_path):
+    if not repo.find_by_path(new_path):
         return new_path
     return generate_path(len)
 
@@ -55,10 +31,8 @@ def fail(status_code, msg, detail=None):
 def ok(body=None):
     if body is None:
         return response(200)
-    elif type(body) == str:
-        return response(200, {"message": "success", "path": body})
     else:
-        return response(200, {"message": "success", "urls": body})
+        return response(200, body.to_serializable())
 
 
 def response(status_code, body=None):
@@ -69,31 +43,13 @@ def response(status_code, body=None):
         "statusCode": status_code
     }
     if body is not None:
-        result['body'] = json.dumps(body)
+        result['body'] = json.dumps(__new_dict_without_nones(body))
 
     return result
 
 
-def get_urls_by_user(user):
-    return [url for url in all_urls() if url["user"] == user]
-
-
-def all_urls():
-    return (url_details(k) for k in list_bucket())
-
-
-def list_bucket():
-    return (obj['Key'] for obj in s3_client.list_objects(Bucket=bucket_name)['Contents'])
-
-
-def url_details(path):
-    response = s3_client.head_object(Bucket=bucket_name, Key=path)
-    return {
-        "path": path,
-        "url": response.get('WebsiteRedirectLocation'),
-        "createdAt": str(response.get('LastModified')),
-        "user":  get_user(response)
-    }
+def __new_dict_without_nones(data):
+    return {k: v for k, v in data.items() if v is not None}
 
 
 def get_body(event):
@@ -104,41 +60,16 @@ def get_user(data):
     try:
         return data['requestContext']['authorizer']['claims']['email']
     except KeyError:
-        try:
-            return data['Metadata']['user']
-        except KeyError:
-            return None
-
-
-def delete_url(event, context):
-    user = get_user(event)
-    path = event['pathParameters']['path']
-    try:
-        obj = s3_client.head_object(Bucket=bucket_name, Key=path)
-        if get_user(obj) != user:
-            return forbidden()
-        s3_client.delete_object(Bucket=bucket_name, Key=path)
-        return ok()
-    except ClientError as ex:
-        error_code = ex.response['Error']['Code']
-        if error_code == '404':
-            return forbidden()
-        return fail(500, "unexpected error", str(ex))
-
-
-def list_urls(event, context):
-    try:
-        return ok(get_urls_by_user(get_user(event)))
-    except Exception as ex:
-        logger.error(ex)
-        return fail(500, "unexpected error", str(ex))
+       return None
 
 
 def shorten_url(event, context):
+    user = get_user(event)
     body = get_body(event)
     origin_url, custom_path = body['url'].strip(), body['custom_path'].strip()
     if custom_path:
-        logger.warning(f'Shorten URL: {origin_url} (custom_path="/{custom_path}")')
+        logger.warning(
+            f'Shorten URL: {origin_url} (custom_path="/{custom_path}")')
     else:
         logger.warning(f'Shorten URL: {origin_url}')
 
@@ -148,22 +79,41 @@ def shorten_url(event, context):
 
     if not custom_path:
         path = generate_path(7)
-    else:
-        if not is_path_free(custom_path):
-            logger.error(f'Path "/{custom_path}" is already in use')
-            return fail(400, "Path is already in use")
+    elif not repo.find_by_path(custom_path):
         path = custom_path
+    else:
+        logger.error(f'Path "/{custom_path}" is already in use')
+        return fail(400, "Path is already in use")
 
     try:
-        redirect = create_redirect_object(path, origin_url)
-        user = get_user(event)
-        if user:
-            redirect['Metadata'] = { 'user': user }
-        upload_redirect_object(redirect)
-        return ok(path)
+        return ok(repo.save(path, origin_url, user))
     except Exception as ex:
         logger.error(ex)
         if type(ex) == ClientError and ex.response['Error']['Code'] == 'InvalidRedirectLocation':
             return fail(400, "URL is invalid", ex.response['Error']['Message'])
         else:
             return fail(500, "Error saving redirect", str(ex))
+
+
+def list_urls(event, context):
+    user = get_user(event)
+    try:
+        return ok(repo.find_by_user(user))
+    except Exception as ex:
+        logger.error(ex)
+        return fail(500, "unexpected error", str(ex))
+
+
+def delete_url(event, context):
+    user = get_user(event)
+    path = event['pathParameters']['path']
+
+    try:
+        url = repo.find_by_path(path)
+        if not url or not url.is_owned_by(user):
+            return forbidden()
+
+        repo.delete(url)
+        return ok()
+    except Exception as ex:
+        return fail(500, "unexpected error", str(ex))
---
+++ src/repo.py
@@ -0,0 +1,77 @@
+import boto3
+import logging
+
+from botocore.exceptions import ClientError
+from os import environ as env
+from collections import namedtuple
+from datetime import datetime
+
+
+s3_client = boto3.client('s3')
+logger = logging.getLogger()
+bucket_name = env['BUCKET_NAME']
+
+ShortUrlBase = namedtuple("ShortUrlBase", ["path", "links_to", "created_at", "owner"])
+
+class ShortUrl(ShortUrlBase):
+    def is_owned_by(self, user):
+        return self.owner == user
+    
+    def to_serializable(self):
+        return self._asdict()
+
+
+class UrlList:
+    def __init__(self, urls):
+        self._urls = urls
+
+    def to_serializable(self):
+        return {
+            "urls": [url.to_serializable() for url in self._urls]
+        }
+
+
+def find_by_path(path):
+    try:
+        response = s3_client.head_object(Bucket=bucket_name, Key=path)
+        links_to = response.get('WebsiteRedirectLocation')
+        created_at = response.get('LastModified')
+        owner = response.get('Metadata', {}).get('user')
+        return ShortUrl(path, links_to, str(created_at), owner)
+    except ClientError as ex:
+        if ex.response['Error']['Code'] == '404':
+            return None
+        raise ex
+
+
+def find_by_user(user):
+    result = []
+    for key in __list_bucket_keys():
+        print(key)
+        url = find_by_path(key)
+        print(url)
+        if url.is_owned_by(user):
+            result.append(url)
+    return UrlList(result)
+
+
+def __list_bucket_keys():
+    return (obj['Key'] for obj in s3_client.list_objects(Bucket=bucket_name)['Contents'])
+
+
+def save(path, links_to, user=None):
+    obj = {
+        'Bucket': bucket_name,
+        'Key': path,
+        'WebsiteRedirectLocation': links_to
+    }
+    if user is not None:
+        obj['Metadata'] = {
+            'user': user
+        }
+    s3_client.put_object(**obj)
+    return ShortUrl(path, links_to, str(datetime.now()), user)
+
+
+def delete(url):
+    s3_client.delete_object(Bucket=bucket_name, Key=url.path)
