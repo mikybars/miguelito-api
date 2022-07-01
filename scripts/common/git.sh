a68fbc4db349741e4968a0730d22a8c81b8201d7
Miguel Ibars <miguelpi@ext.zara.com>
2022-07-01 13:03:04 +0200

    Remove helper scripts as the certificate is now managed by ACM

--- scripts/common/git.sh
+++
@@ -1,9 +0,0 @@
-function git::is_inside_repo() {
-    git rev-parse --git-dir &>/dev/null
-}
-
-function git::get_user_email() {
-    git config user.email
-}
-
-# vim: set expandtab ts=4 ft=bash iskeyword+=\: :
