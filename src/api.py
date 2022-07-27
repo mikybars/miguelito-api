ff7aec8cd042932230a0695933df4c45a2eeb5d0
Miguel Ibars <miguelpi@ext.zara.com>
2022-07-27 15:26:35 +0200

    feat: Add URL in response

--- src/api.py
+++ src/api.py
@@ -12,9 +12,17 @@ root_logger.setLevel(LOGLEVEL)
 handler.setFormatter(logging.Formatter('[%(levelname)-8s] %(message)s'))
 
 
+def get_base_url():
+    return f"https://{env['BUCKET_NAME']}/"
+
+
+def with_url(link):
+    return {'url': f'{get_base_url()}{link.backhalf}', **link.asdict()}
+
+
 def get_info(event, context):
     return {
-        "base_url": f"https://{env['BUCKET_NAME']}/"
+        'base_url': get_base_url()
     }
 
 
@@ -26,13 +34,15 @@ def create_link(event, context):
     }
     try:
         u = Link(**data)
-        return repo.save(u).asdict()
+        return with_url(repo.save(u))
     except repo.DuplicateKey:
         raise ApplicationError('Backhalf is already taken')
 
 
 def list_links(event, context):
-    return {"data": [u.asdict() for u in repo.find_by_user(event['user'])]}
+    return {
+        "data": [with_url(link) for link in repo.find_by_user(event['user'])]
+    }
 
 
 def delete_link(event, context):
@@ -53,7 +63,7 @@ def delete_all(event, context):
 def edit_link(event, context):
     backhalf, user, data = event.values()
     try:
-        return repo.update(backhalf=backhalf, user=user, data=data).asdict()
+        return with_url(repo.update(backhalf=backhalf, user=user, data=data))
     except repo.KeyNotFound:
         raise AuthorizationError('forbidden')
     except repo.DuplicateKey:
